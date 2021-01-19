eaa700103e38e3ec7544dfdb7947c3a2d60bbd8f
Gabriel Pivato <pivatogabriel@gmail.com>
2021-01-18 22:39:51 -0300

    increased rpc callbacks number to a bigger value

--- src/ThingsBoard.h
+++ src/ThingsBoard.h
@@ -371,7 +371,7 @@ private:
   }
 
   PubSubClient m_client;              // PubSub MQTT client instance.
-  RPC_Callback m_rpcCallbacks[8];     // RPC callbacks array
+  RPC_Callback m_rpcCallbacks[24];     // RPC callbacks array
 
   // PubSub client cannot call a method when message arrives on subscribed topic.
   // Only free-standing function is allowed.
